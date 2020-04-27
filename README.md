1666a38ed7a7248de822b2d7708667cade67fe28
Jiang-Jiang Liu <j04.liu@gmail.com>
2020-04-27 21:26:56 +0800

    Init

---
+++ README.md
@@ -0,0 +1,64 @@
+# SCNet
+The official pytorch implemention of CVPR 2020 paper ["Improving Convolutional Networks with Self-Calibrated Convolutions"](http://mftp.mmcheng.net/Papers/20cvprSCNet.pdf)
+
+## Introduction
+we present a novel self-calibrated convolution that explicitly expands fields-of-view of each convolutional layer
+through internal communications and hence enriches the
+output features. In particular, unlike the standard convolutions that fuse spatial and channel-wise information using
+small kernels (e.g., 3 × 3), our self-calibrated convolution
+adaptively builds long-range spatial and inter-channel dependencies around each spatial location through a novel
+self-calibration operation. Thus, it can help CNNs generate
+more discriminative representations by explicitly incorporating richer information. Our self-calibrated convolution
+design is simple and generic, and can be easily applied to
+augment standard convolutional layers without introducing
+extra parameters and complexity. Extensive experiments
+demonstrate that when applying our self-calibrated convolution into different backbones, the baseline models can be
+significantly improved in a variety of vision tasks, including image recognition, object detection, instance segmentation, and keypoint detection, with no need to change network architectures.
+<div align="center">
+  <img src="https://github.com/backseason/SCNet/blob/master/figures/SC-Conv.png">
+</div>
+<p align="center">
+  Figure 1: Diagram of self-calibrated convolution.
+</p>
+
+## Useage
+### Requirement
+PyTorch>=0.4.1
+### Examples 
+```
+git clone https://github.com/backseason/SCNet.git
+
+from scnet import scnet50
+model = scnet50(pretrained=True)
+
+```
+Input image should be normalized as follows:
+```
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                  std=[0.229, 0.224, 0.225])
+```
+
+(The pretrained model should be downloaded automatically by default.
+You may also choose to download them manually by the links listed below.)
+
+## Pretrained models
+| model |#Params | MAdds | FLOPs |top-1 error| top-5 error| Link 1 | Link 2 |
+| :--: | :--: | :--: | :--: | :--: | :--: | :--: |
+| SCNet-50  | 25.56M | 4.0G | 7.9G  | 22.19 | 6.08 |[GoogleDrive](https://drive.google.com/open?id=1rA266TftaUymbtPTVHCJYoxDwl6K4gLr) | [BaiduYun](https://pan.baidu.com/s/13js74yBkCsGAFx6N8ki7UA) password: **95p5**
+| SCNet-101 | 44.57M | 7.2G | 14.4G | 21.06 | 5.75 |[GoogleDrive](https://drive.google.com/open?id=11-rW7l9vl-HGrOoCktEjRBPxMeKw334x) | [BaiduYun](https://pan.baidu.com/s/1qtwTxKbhzdxYqADsbgCcpQ) password: **38oh**
+
+## Applications
+Other applications such as Classification, Instance segmentation, Object detection, Semantic segmentation, and Human keypoint detection can be found on https://mmcheng.net/scconv/.
+
+## Citation
+If you find this work or code is helpful in your research, please cite:
+```
+@inproceedings{liu2020scnet,
+ title={Improving Convolutional Networks with Self-Calibrated Convolutions},
+ author={Jiang-Jiang Liu and Qibin Hou and Ming-Ming Cheng and Changhu Wang and Jiashi Feng},
+ booktitle={IEEE CVPR},
+ year={2020},
+}
+```
+## Contact
+If you have any questions, feel free to contact me via: `j04.liu(at)gmail.com`.
---
+++ scnet.py
@@ -0,0 +1,195 @@
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.model_zoo as model_zoo
+
+
+__all__ = ['SCNet', 'scnet50', 'scnet101']
+
+
+model_urls = {
+    'scnet50': 'https://backseason.oss-cn-beijing.aliyuncs.com/scnet/scnet101-44c5b751.pth',
+    'scnet101': 'https://backseason.oss-cn-beijing.aliyuncs.com/scnet/scnet101-44c5b751.pth',
+}
+
+def conv3x3(in_planes, out_planes, stride=1, groups=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=1, groups=groups, bias=False)
+
+
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+
+class SCConv(nn.Module):
+    def __init__(self, planes, stride, pooling_r):
+        super(SCConv, self).__init__()
+        self.k2 = nn.Sequential(
+                    nn.AvgPool2d(kernel_size=pooling_r, stride=pooling_r), 
+                    conv3x3(planes, planes), 
+                    nn.BatchNorm2d(planes),
+                    )
+        self.k3 = nn.Sequential(
+                    conv3x3(planes, planes), 
+                    nn.BatchNorm2d(planes),
+                    )
+        self.k4 = nn.Sequential(
+                    conv3x3(planes, planes, stride), 
+                    nn.BatchNorm2d(planes),
+                    nn.ReLU(inplace=True),
+                    )
+
+    def forward(self, x):
+        identity = x
+
+        out = torch.sigmoid(torch.add(identity, F.interpolate(self.k2(x), identity.size()[2:]))) # sigmoid(identity + k2)
+        out = torch.mul(self.k3(x), out) # k3 * sigmoid(identity + k2)
+        out = self.k4(out) # k4
+
+        return out
+
+
+class SCBottleneck(nn.Module):
+    expansion = 4
+    pooling_r = 4 # down-sampling rate of the avg pooling layer in the K3 path of SC-Conv.
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(SCBottleneck, self).__init__()
+        planes = int(planes / 2)
+
+        self.conv1_a = conv1x1(inplanes, planes)
+        self.bn1_a = nn.BatchNorm2d(planes)
+
+        self.k1 = nn.Sequential(
+                    conv3x3(planes, planes, stride), 
+                    nn.BatchNorm2d(planes),
+                    nn.ReLU(inplace=True),
+                    )
+
+        self.conv1_b = conv1x1(inplanes, planes)
+        self.bn1_b = nn.BatchNorm2d(planes)
+        
+        self.scconv = SCConv(planes, stride, self.pooling_r)
+
+        self.conv3 = conv1x1(planes * 2, planes * 2 * self.expansion)
+        self.bn3 = nn.BatchNorm2d(planes * 2 * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out_a= self.conv1_a(x)
+        out_a = self.bn1_a(out_a)
+        out_a = self.relu(out_a)
+
+        out_a = self.k1(out_a)
+
+
+        out_b = self.conv1_b(x)
+        out_b = self.bn1_b(out_b)
+        out_b = self.relu(out_b)
+
+        out_b = self.scconv(out_b)
+
+        out = self.conv3(torch.cat([out_a, out_b], dim=1))
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+
+class SCNet(nn.Module):
+    def __init__(self, block, layers, num_classes=1000):
+        super(SCNet, self).__init__()
+        self.inplanes = 64
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(self.inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+
+def scnet50(pretrained=False, **kwargs):
+    """Constructs a SCNet-50 model.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = SCNet(SCBottleneck, [3, 4, 6, 3], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['scnet50']))
+    return model
+
+
+def scnet101(pretrained=False, **kwargs):
+    """Constructs a SCNet-101 model.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = SCNet(SCBottleneck, [3, 4, 23, 3], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['scnet101']))
+    return model
+
+
+if __name__ == '__main__':
+    images = torch.rand(1, 3, 224, 224).cuda(0)
+    model = scnet101(pretrained=True)
+    model = model.cuda(0)
+    print(model(images).size())
